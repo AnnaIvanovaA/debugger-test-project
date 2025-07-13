421fb9705bcf71378eb4a930306342da5f03cf16
Anna Ivanova <anna.ivanova@jetbrains.com>
2025-07-13 16:24:08 +0200

    pushed .idea

--- src/main/java/stream/Stream.java
+++ src/main/java/stream/Stream.java
@@ -1,4 +1,9 @@
 package stream;
 
+import java.util.List;
+
 public class Stream {
+    public static void main(String[] args) {
+        List.of(1,2,3,4,5,10).stream().filter(i -> i % 2 == 0).limit(19).toList();
+    }
 }
